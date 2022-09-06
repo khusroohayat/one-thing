3104544b720dd9ff2d48b40df402a888e01c37af
khusroo hayat <khusroo.hayat@gmail.com>
2022-09-06 13:06:55 +0500

    Add components and finalize app

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,63 @@
+// react imports
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
 
+// libraries
+import JSConfetti from 'js-confetti';
+const jsConfetti = new JSConfetti()
+jsConfetti.addConfetti()
+
+// custom components
+import CustomForm from "./components/CustomForm";
+import OneThing from './components/OneThing';
+
+function getSuccessMessage() {
+  const messages = ["Congrats!", "Great job!", "Don't ya feel great?!"];
+  return messages[Math.floor(Math.random() * messages.length)];
+}
 function App() {
-  const [count, setCount] = useState(0)
+  const [thing, setThing] = useState("")
+  const [isCompleted, setIsCompleted] = useState(true)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsCompleted(false);
+  }
+
+  const handleInput = (e) => {
+    setThing(e.target.value);
+  }
+
+  const handleCompletedThing = async (e) => {
+    e.target.setAttribute('disabled', true);
+    setThing(getSuccessMessage());
+    await jsConfetti.addConfetti({
+      emojis: ['🌈', '⚡️', '💥', '✨', '💫', '🌸']
+    })
+    e.target.removeAttribute('disabled');
+    setThing("");
+    setIsCompleted(true);
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main className="grid place-items-center min-h-screen bg-gradient-to-b from-slate-100 to-slate-200 dark:from-slate-800 dark:to-slate-900 text-slate-900 dark:text-slate-200">
+      <div className="grid place-items-center gap-8 m-8">
+        { isCompleted && (
+          <CustomForm 
+            thing = {thing}
+            handleInput = {handleInput}
+            handleSubmit = {handleSubmit}
+          />
+        )}
+        {
+          !isCompleted && <OneThing 
+            thing = {thing}
+            handleCompletedThing = {handleCompletedThing}
+          />
+        }
+        
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+      
+    </main>
   )
 }
 
